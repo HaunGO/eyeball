48f61865b7886e394c3f00989f536f1905da0a07
HaunGO <haungo@gmail.com>
2014-10-13 16:47:06 -0400

    Major updates to plugin structure and samples.

--- eyeball/eyeball.js
+++ eyeball/eyeball.js
@@ -28,8 +28,8 @@
           pupilColor: "#333",
           size: '100'
         },
-        EYEBALL;
-
+        EYEBALL,
+        dataKey = "plugin_" + pluginName;
 
 
 
@@ -44,11 +44,10 @@
         this._defaults = defaults;
         this._name = pluginName;
         this.init();
+
+        // console.log(element);
+    
     }
-
-
-
-
 
 
 
@@ -56,8 +55,8 @@
 
     // Avoid Plugin.prototype conflicts
     $.extend(Plugin.prototype, {
-        init: function () 
-        {
+        
+        init: function() {
             // Place initialization logic here
             // You already have access to the DOM element and
             // the options via the instance, e.g. this.element
@@ -65,27 +64,36 @@
             // you can add more functions like the one below and
             // call them like so: this.yourOtherFunction(this.element, this.settings).
 
-            console.log("eyeball created!");
             // console.log(this.settings);
             
             EYEBALL = $(this.element);
 
+            this.buildEyeBall();
             this.buildParallax();
             this.setColors();
             this.setSize();
 
+            console.log("eyeball created!");
+
         },
-        buildParallax: function() {
+
+        buildEyeBall: function() {
             // ADD THE NESTED DIVS WITH THE CLASS AND DATA FOR THE PARALLAX.JS
-            var BallIrisPupil =  $("<div class='layer ball'  data-depth='-.5'></div><div class='layer iris'  data-depth='-2'></div><div class='layer pupil' data-depth='-2.1'></div>");
+            // var BallIrisPupil =  $("<div class='layer ball'  data-depth='-.5'></div><div class='layer iris'  data-depth='-2'></div><div class='layer pupil' data-depth='-2.1'></div>");
+            var BallIrisPupil =  $("<div class='eyeball'><div class='layer ball'  data-depth='-.5'></div><div class='layer iris'  data-depth='-2'></div><div class='layer pupil' data-depth='-2.1'></div></div>");
+            // var BallIrisPupil =  $("<div class='eyeball'><div class='ball'></div><div class='iris'></div><div class='pupil'></div></div>");
 
             EYEBALL.append(BallIrisPupil);
+
+        },
+
+        buildParallax: function() {
 
             EYEBALL.parallax({
                relativeInput:true,
                clipRelativeInput:true,
-               limitY:40,
-               limitX:40,
+               limitY:50,
+               limitX:50,
                frictionX:0.4, 
                frictionY:0.4
             });
@@ -95,16 +103,17 @@
             }  
 
         },
-        // blink: function() 
-        // {
-        //     console.log('>blink<');
-        // },
+
+        blink: function() {
+            console.log('>blink<');
+        },
+
         setColors: function() 
         {
             // IF THERE ARE CUSTOM SETTING SENT VIA CONSTRUCTOR FUNCTION, 
             // IT WILL OVERWRITE THE DATA-COLOR ATTRIBUTE:
             if(this.settings.irisColor === defaults.irisColor){  
-              var color = EYEBALL.data('color');
+              var color = EYEBALL.data('eye-color');
               if(color !== undefined) {          
                 this.settings.irisColor = color;
               }
@@ -116,12 +125,13 @@
             });
 
         },
+
         setSize: function() 
         {
             // IF THERE ARE CUSTOM SETTING SENT VIA CONSTRUCTOR FUNCTION, 
             // IT WILL OVERWRITE THE DATA-COLOR ATTRIBUTE:
             if(this.settings.size === defaults.size){  
-              var size = EYEBALL.data('size');
+              var size = EYEBALL.data('eye-size');
               if(size !== undefined) {          
                 this.settings.size = size;
               }
@@ -132,7 +142,14 @@
               'width': this.settings.size + 'px',
               'height': this.settings.size + 'px'
             });
-        },
+
+            EYEBALL.find('.eyeball').css({
+              'width': this.settings.size + 'px',
+              'height': this.settings.size + 'px'
+            });
+
+            // console.log(EYEBALL)
+        }
 
 
 
@@ -143,43 +160,74 @@
 
 
 
+/*
+*   THIS IS THE PLUGIN WRAPPER CODE THAT CAME IN THE jquery.boilerplate THAT I USED,
+*   BUT IT WAS SOMEHOW PREVENTING ME FROM CALLING METHODS/FUNCTIONS 
+*   FROM OUTSIDE OF THE PLUGIN.   
+*
+*   THE ALTERNATE VERSION WAS PULLED FROM A FUNCTIONING EXAMPLE FROM THE NET..
+*   I DON'T UNDERSTAND IT.. .. .. PERHAPSE, ONE DAY I WILL. 
+*
+*   THE ALTERNATE VERSION, THAT I AM NOW USING, SOMEHOW CHANGES THE FUNCTIONALITY OF
+*   HOW THE PLUGIN CAN BE CALLED.. ..  CANNOT APPLY TO MULTIPLE, 
+*   eg: $('.eyeball') APPLIES THE SAME SETTING TO EACH. 
+* 
+*/
+
+// FIRST WRAPPER CODE:
+    // // A really lightweight plugin wrapper around the constructor,
+    // // preventing against multiple instantiations
+    //  $.fn[ pluginName ] = function ( options ) {
+    //     this.each(function() {
+    //         if ( !$.data( this, "plugin_" + pluginName ) ) {
+    //             $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+    //         }
+    //     });
+
+    //     // chain jQuery functions
+    //     return this;
+    // };
 
 
-// var blink = function() {
-//   console.log('>blink<');
-// };
 
 
-// function blink() {
-//   console.log('>blink<');
-// }
+// ALTERNATE WRAPPER CODE THAT I PULLED FROM ANOTHER EXAMPLE:
+    /*
+     * Plugin wrapper, preventing against multiple instantiations and
+     * return plugin instance.
+     */
+    $.fn[pluginName] = function (options) {
 
+        var plugin = this.data(dataKey);
 
-
-
-
-
-
-
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
-    $.fn[ pluginName ] = function ( options ) {
-        this.each(function() {
-            if ( !$.data( this, "plugin_" + pluginName ) ) {
-                $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+        // has plugin instantiated ?
+        if (plugin instanceof Plugin) {
+            // if have options arguments, call plugin.init() again
+            if (typeof options !== 'undefined') {
+                plugin.init(options);
             }
-        });
-
-
-        // chain jQuery functions
-        return this;
+        } else {
+            plugin = new Plugin(this, options);
+            this.data(dataKey, plugin);
+        }
+        
+        return plugin;
     };
 
 
 
 
 
-})( jQuery, window, document );
+
+
+
+
+
+
+
+
+
+})(jQuery, window, document);
 
 
 
